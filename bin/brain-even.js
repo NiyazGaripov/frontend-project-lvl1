108eba732b6803f758b989cc24b5b080dae2a420
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-18 15:44:01 +0300

    feat: create brain-even.js file, add DI, create constants and variables

---
+++ bin/brain-even.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const MAX_NUMBER = 100;
+const DIVIDER = 2;
+const QUESTION_COUNT = 3;
+const STEP = 1;
+let CORRECT_ANSWER_COUNT = 1;
+let userName = '';
