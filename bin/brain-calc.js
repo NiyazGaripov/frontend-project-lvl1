df9fa6bcbd3270e3e284d60b95a8ef9b78dd9c07
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-20 10:41:44 +0300

    feat: create function askQuestions

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -21,3 +21,44 @@ const greeting = () => {
   userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+const askQuestions = () => {
+  while (CORRECT_ANSWER_COUNT <= QUESTION_COUNT) {
+    const firstOperand = getRandomNumber();
+    const secondOperand = getRandomNumber();
+    const operator = getRandomArrayItem(OPERATORS);
+    let rightAnswer;
+
+    switch (operator) {
+      case '+':
+        rightAnswer = firstOperand + secondOperand;
+        break;
+      case '-':
+        rightAnswer = firstOperand - secondOperand;
+        break;
+      case '*':
+        rightAnswer = firstOperand * secondOperand;
+        break;
+      default:
+        return;
+    }
+
+    const userAnswer = readlineSync.questionInt(`Question: ${firstOperand} ${operator} ${secondOperand}\nYour answer: `);
+
+    if (rightAnswer === userAnswer) {
+      console.log('Correct!');
+
+      if (CORRECT_ANSWER_COUNT === QUESTION_COUNT) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+
+      CORRECT_ANSWER_COUNT += STEP;
+    } else if (rightAnswer !== userAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
+      return;
+    } else {
+      console.log(`Please use numbers\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+};
