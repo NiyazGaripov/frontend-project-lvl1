6fb7fa9f83f7beedfd49fb3096719c7bf4d3bbf8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-20 10:40:14 +0300

    feat: create auxiliary functions

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -8,3 +8,11 @@ const STEP = 1;
 const OPERATORS = ['+', '-', '*'];
 let CORRECT_ANSWER_COUNT = 1;
 let userName = '';
+
+const getRandomNumber = (maxCount = MAX_NUMBER) => Math.floor(Math.random() * maxCount);
+
+const getRandomArrayItem = (array) => {
+  const randomItem = getRandomNumber(array.length);
+
+  return array[randomItem];
+};
