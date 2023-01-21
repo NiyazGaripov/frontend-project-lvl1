8000e73d991ab9ea51d931d64d788a3dda851beb
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-21 15:07:18 +0300

    feat: create function buildGame

--- src/index.js
+++ src/index.js
@@ -11,6 +11,32 @@ const greeting = () => {
   console.log(`Hello, ${userName}!`);
 };
 
+const buildGame = (gameConfig) => {
+  const { description, gameData } = gameConfig;
+  console.log(description);
+
+  for (let index = 0; index < gameData.length; index += 1) {
+    const { question, rightAnswer } = gameData[index];
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+
+      if (CORRECT_ANSWER_COUNT === QUESTION_COUNT) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+
+      CORRECT_ANSWER_COUNT += STEP;
+    } else if (userAnswer !== rightAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
+      return;
+    } else {
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+};
+
 const startGame = (gameConfig) => {};
 
 export default startGame;
