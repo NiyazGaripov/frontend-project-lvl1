9bf3ff6d465405a2a390e2b5a117130e9c48b523
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-21 15:06:59 +0300

    feat: create function greeting

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const STEP = 1;
 let CORRECT_ANSWER_COUNT = 1;
 let userName = '';
 
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
 const startGame = (gameConfig) => {};
 
 export default startGame;
