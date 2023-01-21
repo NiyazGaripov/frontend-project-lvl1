e14d23d43fdac661fc406c7c844e93c0ad32413f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-21 15:05:57 +0300

    feat: add const and variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import readlineSync from 'readline-sync';
 import QUESTION_COUNT from './constants.js';
 
+const STEP = 1;
+let CORRECT_ANSWER_COUNT = 1;
+let userName = '';
+
 const startGame = (gameConfig) => {};
 
 export default startGame;
