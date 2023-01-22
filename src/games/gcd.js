356ec74246b7a18bd07600beb16a6b2328267222
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-22 08:57:08 +0300

    feat: create function getGreatestCommonDivisor

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,3 +3,9 @@ import { QUESTION_COUNT, MAX_NUMBER } from '../constants.js';
 import startGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
+
+const getGreatestCommonDivisor = (firstOperand, secondOperand) => {
+  const remainder = firstOperand % secondOperand;
+
+  return secondOperand ? getGreatestCommonDivisor(secondOperand, remainder) : firstOperand;
+};
