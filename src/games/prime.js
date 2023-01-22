1083b6a6503f50e0d058d8554fc5d883c4dca01f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-22 22:41:16 +0300

    feat: add DI and constants

---
+++ src/games/prime.js
@@ -0,0 +1,6 @@
+import { getRandomNumber } from '../utils.js';
+import { QUESTION_COUNT, MAX_NUMBER } from '../constants.js';
+import startGame from '../index.js';
+
+const START_NUMBER = 2;
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
