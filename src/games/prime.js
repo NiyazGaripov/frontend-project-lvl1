ab0a95c5c640befb4758e4cbae6efdbee99ca48d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-22 22:41:50 +0300

    feat: create function isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,3 +4,15 @@ import startGame from '../index.js';
 
 const START_NUMBER = 2;
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (value) => {
+  const squareRoot = Math.sqrt(value);
+
+  for (let i = START_NUMBER; i <= squareRoot; i += 1) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+
+  return value > 1;
+};
