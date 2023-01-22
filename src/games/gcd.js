004c5a632ea206a86cc45cefe3a47d714cfc6e9f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-22 08:56:41 +0300

    feat: add const description

---
+++ src/games/gcd.js
@@ -0,0 +1,5 @@
+import { getRandomNumber } from '../utils.js';
+import { QUESTION_COUNT, MAX_NUMBER } from '../constants.js';
+import startGame from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
