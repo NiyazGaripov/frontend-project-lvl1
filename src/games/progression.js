6ed2316c94b20e1b177460e2c134cb73b5b972d6
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-22 17:04:45 +0300

    feat: create function getArithmeticProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,3 +7,12 @@ const MAX_NUMBER = 8;
 const PROGRESSION_LENGTH = 10;
 const DELETE_COUNT = 1;
 const description = 'What number is missing in the progression?';
+
+const getArithmeticProgression = () => {
+  const NTH_NUMBER_OF_PROGRESSION = getRandomNumberBetweenTwoValues(MIN_NUMBER, MAX_NUMBER);
+  const STEP_OF_PROGRESSION = getRandomNumberBetweenTwoValues(MIN_NUMBER, MAX_NUMBER);
+
+  return Array(PROGRESSION_LENGTH)
+    .fill()
+    .map((item, i) => (i + NTH_NUMBER_OF_PROGRESSION) * STEP_OF_PROGRESSION);
+};
