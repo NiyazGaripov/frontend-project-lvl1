1061f9440185fd2cde29db96f76d6fcbb6d377d5
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-25 21:27:23 +0300

    refactor: transfer all logic inside main function, remove variables at module level, remove increments, nested conditions, and comparisons, put question in separate console.log

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
 import readlineSync from 'readline-sync';
-import { QUESTION_COUNT } from './constants.js';
 
-const STEP = 1;
-let CORRECT_ANSWER_COUNT = 1;
-let userName = '';
-
-const greeting = () => {
+const startGame = (gameConfig) => {
   console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-};
 
-const buildGame = (gameConfig) => {
   const { description, gameData } = gameConfig;
   console.log(description);
 
   for (let index = 0; index < gameData.length; index += 1) {
     const { question, rightAnswer } = gameData[index];
-    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === rightAnswer) {
       console.log('Correct!');
-
-      if (CORRECT_ANSWER_COUNT === QUESTION_COUNT) {
-        console.log(`Congratulations, ${userName}!`);
-      }
-
-      CORRECT_ANSWER_COUNT += STEP;
-    } else if (userAnswer !== rightAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
-      return;
     } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
       console.log(`Let's try again, ${userName}!`);
-      return;
+      return false;
     }
   }
-};
 
-const startGame = (gameConfig) => {
-  greeting();
-  buildGame(gameConfig);
+  console.log(`Congratulations, ${userName}!`);
+  return true;
 };
 
 export default startGame;
