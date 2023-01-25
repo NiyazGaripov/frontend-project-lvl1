e8152480d3b1fda53edbe2d080c18260c91ede24
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-25 12:19:20 +0300

    refactor: rename const, cut off borderline cases using Guard Expression

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,19 +2,23 @@ import { getRandomNumber } from '../utils.js';
 import { QUESTION_COUNT, MAX_NUMBER } from '../constants.js';
 import startGame from '../index.js';
 
-const START_NUMBER = 2;
+const FIRST_PRIME_NUMBER = 2;
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (value) => {
+  if (value < FIRST_PRIME_NUMBER) {
+    return false;
+  }
+
   const squareRoot = Math.sqrt(value);
 
-  for (let i = START_NUMBER; i <= squareRoot; i += 1) {
+  for (let i = FIRST_PRIME_NUMBER; i <= squareRoot; i += 1) {
     if (value % i === 0) {
       return false;
     }
   }
 
-  return value > 1;
+  return true;
 };
 
 const gameData = new Array(QUESTION_COUNT).fill('').map(() => {
