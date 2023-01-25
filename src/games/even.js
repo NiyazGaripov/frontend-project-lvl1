3ac42540ce8b5c0ba57ee883b437070061d4c571
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-25 10:34:09 +0300

    refactor: rename const, create function isEven, transfers calculate answer to constant

--- src/games/even.js
+++ src/games/even.js
@@ -2,15 +2,17 @@ import { getRandomNumber } from '../utils.js';
 import { QUESTION_COUNT, MAX_NUMBER } from '../constants.js';
 import startGame from '../index.js';
 
-const DIVIDER = 2;
+const NUMBER_BY_WHICH_DIVIDE = 2;
 const description = 'Answer "yes" if the number is even, otherwise answer "no"';
+const isEven = (number) => number % NUMBER_BY_WHICH_DIVIDE === 0;
 
 const gameData = new Array(QUESTION_COUNT).fill('').map(() => {
   const randomNumber = getRandomNumber(MAX_NUMBER);
+  const rightAnswer = isEven(randomNumber) ? 'yes' : 'no';
 
   return {
     question: String(randomNumber),
-    rightAnswer: randomNumber % DIVIDER === 0 ? 'yes' : 'no',
+    rightAnswer,
   };
 });
 
